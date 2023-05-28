221dd174d382aa900844350ad6d12e1b2ace74e1
FrantiskaAli <tiskarechk@gmail.com>
2023-05-28 23:16:13 +0100

    validation of the email added in js, css styling for error messages added

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('i am linked properly')
+//checking input is valid email
+const email = $("#email-address")
+
+
+$("#submit-btn").on('click', function () {
+   $('.error').remove()
+    const emailValue = email.val()
+    let re = new RegExp();
+    re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (re.test(emailValue)) {
+        alert("valid email");
+
+    } else {
+        //alert('invalid email');
+        email.addClass('invalid-input')
+        $('<label for="email-address" class="error">Valid email required</label>').insertAfter($('#email-label'))
+
+    }
+
+})
+
+//function from stack overflow
