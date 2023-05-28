3305b2faa62bc9c5afcce02a40f898ac8f974536
FrantiskaAli <tiskarechk@gmail.com>
2023-05-29 00:02:06 +0100

    started edditing success msg

--- script.js
+++ script.js
@@ -8,8 +8,14 @@ $("#submit-btn").on('click', function () {
     let re = new RegExp();
     re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     if (re.test(emailValue)) {
-        alert("valid email");
-
+        //alert("valid email");
+        $('#content-window').empty();
+        const image = $("<img>").attr('src','./assets/images/icon-success.svg').addClass('success-img');
+        const heading = $("<h1>").text("Thanks for subscribing!");
+        const message = $(`<p> A confirmation email has been sent to <strong>${emailValue}</strong> 
+        Please open it and click the button inside to confirm your subscription.</p>`);
+        const dismiss = $('<button>').addClass("btn success-btn").text('Dismiss message');
+        $('#content-window').append(image, heading, message, dismiss).addClass("success")
     } else {
         //alert('invalid email');
         email.addClass('invalid-input')
