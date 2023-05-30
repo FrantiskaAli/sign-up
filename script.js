d50f1e1db63e0cbe77a88f801fb8305b173e0541
FrantiskaAli <tiskarechk@gmail.com>
2023-05-30 22:11:35 +0100

    in this commit i have done work on media querries

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ $("#submit-btn").on('click', function () {
         //alert("valid email");
         $('#content-window').empty();
         const image = $("<img>").attr('src','./assets/images/icon-success.svg').addClass('success-img');
-        const heading = $("<h1>").text("Thanks for subscribing!");
-        const message = $(`<p> A confirmation email has been sent to <strong>${emailValue}</strong> 
+        const heading = $("<h1>").text("Thanks for subscribing!").addClass('tyH');
+        const message = $(`<p class="thanks-text"> A confirmation email has been sent to <strong>${emailValue}</strong> 
         Please open it and click the button inside to confirm your subscription.</p>`);
-        const dismiss = $('<button>').addClass("btn success-btn").text('Dismiss message');
+        const dismiss = $('<button>').addClass("btn success-btn").text('Dismiss message').attr("id","dismiss");
         $('#content-window').append(image, heading, message, dismiss).addClass("success")
     } else {
         //alert('invalid email');
@@ -24,5 +24,7 @@ $("#submit-btn").on('click', function () {
     }
 
 })
-
-//function from stack overflow
+//dismiss button
+$("#dismiss").click(function() {
+    window.location.href = "https://www.frontendmentor.io/profile/FrantiskaAli";
+})
